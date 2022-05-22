5d1738e2ac03477b7c2e1b43853d0cf395e1cc47
obleus <odney@Odneys-MacBook-Air.local>
2022-05-22 12:12:01 -0400

    code is broke node app.js is not working

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 const teamMembers = [];
+
 const mysql = require('mysql2');
 require('console.table');
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -46,15 +48,14 @@ const promptMenu = () => {
             }
         });
 };
-
 const selectDepartments = () => {
     connection.query(
         'SELECT * FROM department;',
         (err, results) => {
             console.table(results); 
             promptMenu();
-        }
-    )
+        });
+
 };
 const selectRoles = () => {
     connection.query(
@@ -69,48 +70,228 @@ const selectEmployees = () => {
     connection.query(
         "SELECT E.id, E.first_name, E.last_name, R.title, D.name AS department, R.salary, CONCAT(M.first_name,' ',M.last_name) AS manager FROM employee E JOIN role R ON E.role_id = R.id JOIN department D ON R.department_id = D.id JOIN employee M ON E.manager_id = M.id;",
         (err, results) => {
-            console.table(results);
+            console.table(results); 
             promptMenu();
         }
     )
 };
 
-const promptAddDepartment = () => {
-    return inquirer.prompt([
+const promptAddDepartment = (departmentChoices) => {
+
+    inquirer.prompt([
         {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of the department? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the name of your department!');
-                    return false;
-                }
-            }
+            type: 'list',
+            name: 'departmentName',
+            message: 'What is the name of your department? (Required)',
+            choices: departmentChoices
         },
     ]).then(answers => {
-        console.log(answers);
-        const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
-        teamMembers.push(manager);
-        promptMenu();
-    })
+        console.log("answers", answers.departmentName);
+
+        var query =
+            `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+  FROM employee e
+  JOIN role r
+	ON e.role_id = r.id
+  JOIN department d
+  ON d.id = r.department_id
+  WHERE d.id = ?`
+
+        connection.query(query, answers.departmentName, function (err, res) {
+            if (err) throw err;
+
+            console.table("response ", res);
+            console.log(res.affectedRows + "Employees are viewed!\n");
+
+            promptMenu();
+        })
+    });
 }
+
+// 
 
 const promptAddRole = () => {
+    return connection.promise().query(
+        "SELECT department.id, department.name FROM department;"
+    )
+        .then(([departments]) => {
+            let departmentChoices = departments.map(({
+                id,
+                name
+            }) => ({
+                name: name,
+                value: id
+            }));
 
-    function employeeFirstName() {
-        var employees = connection.query('SELECT first_name FROM employee;')
-        for (var i = 0; i < employees.length; i++) {
-            var employee = employees[i];
-            employees.map(employee);
-            console.log(employee);
-        }
-    }
+            inquirer.prompt(
+                [{
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter the name of your title (Required)',
+                    validate: titleName => {
+                        if (titleName) {
+                            return true;
+                        } else {
+                            console.log('Please enter your title name!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'Which department are you from?',
+                    choices: departmentChoices
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'Enter your salary (Required)',
+                    validate: salary => {
+                        if (salary) {
+                            return true;
+                        } else {
+                            console.log('Please enter your salary!');
+                            return false;
+                        }
+                    }
+                }
+                ]
+            )
+                .then(roles => {
+                    console.log(roles);
+                });
 
-    employeeFirstName();
+        })
 }
 
+
+const promptAddEmployee = () => {
+
+    return connection.promise().query(
+        "SELECT R.id, R.title FROM role R;"
+    )
+        .then(([employees]) => {
+            let titleChoices = employees.map(({
+                id,
+                title
+
+            }) => ({
+                title: id,
+                value: title
+            }))
+
+            inquirer.prompt(
+                [{
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'What is the employees first name (Required)',
+                    validate: firstName => {
+                        if (firstName) {
+                            return true;
+                        } else {
+                            console.log('Please enter the employees first name!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'What is the employees last name (Required)',
+                    validate: lastName => {
+                        if (lastName) {
+                            return true;
+                        } else {
+                            console.log('Please enter the employees last name!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'employeesRole',
+                    message: 'What is the employees role?',
+                    choices: titleChoices
+                },
+                {
+                    type: 'input',
+                    name: 'manager',
+                    message: 'Who is the employees manager? (Required)',
+                    validate: manager => {
+                        if (manager) {
+                            return true;
+                        } else {
+                            console.log('Please enter your employees manager!');
+                            return false;
+                        }
+                    }
+                }]
+
+            ).then(employees => {
+                console.table(employees);
+                console.log(employees + "inserted successfully!\n");
+            });
+
+        })
+}
+
+const promptUpdateRole = () => {
+
+    return connection.promise().query(
+        "SELECT department.id, department.name FROM department;"
+    )
+        .then(([departments]) => {
+            let departmentChoices = departments.map(({
+                id,
+                name
+            }) => ({
+                name: name,
+                value: id
+            }));
+
+            inquirer.prompt(
+                [{
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter the name of your title (Required)',
+                    validate: titleName => {
+                        if (titleName) {
+                            return true;
+                        } else {
+                            console.log('Please enter your title name!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'Which department are you from?',
+                    choices: departmentChoices
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'Enter your salary (Required)',
+                    validate: salary => {
+                        if (salary) {
+                            return true;
+                        } else {
+                            console.log('Please enter your salary!');
+                            return false;
+                        }
+                    }
+                }
+                ]
+            )
+                .then(roles => {
+                
+                    console.log(roles);
+                  
+                });
+
+        })
+}
 
 promptMenu();
